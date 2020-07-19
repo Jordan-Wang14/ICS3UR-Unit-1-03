65c813cb10a4dab228f185bf53cf228dbedcb2e8
Ubuntu <ubuntu@ip-172-31-90-171.ec2.internal>
2020-07-19 16:38:44 +0000

    ICS3U-Unit1-03n

---
+++ Hello_World.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+#created by Jordan Wang
+#created on July 2020
+#This is the "Hello,World!"program,with proper style
+
+
+def main():
+    print("Hello,World")
+    
+
+if __name__ == "__main__":
+    main()
